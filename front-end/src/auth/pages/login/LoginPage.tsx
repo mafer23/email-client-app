079280ac8c31e7c44bdc9bccc10efc3e56fc6c3a
Mateo8585 <42383450+Mateo8585@users.noreply.github.com>
2024-02-20 16:54:40 -0500

    add: rounting system added

---
+++ front-end/src/auth/pages/login/LoginPage.tsx
@@ -0,0 +1,9 @@
+import "./_loginPage.scss";
+
+export const LoginPage = () => {
+  return (
+    <div className="loginPage">
+        LoginPage
+    </div>
+  );
+}
--- front-end/src/auth/routes/AuthRoutes.tsx
+++ front-end/src/auth/routes/AuthRoutes.tsx
@@ -2,12 +2,13 @@
 import { Route, Routes } from 'react-router';
 
 //* Importing pages
+import { LoginPage } from '../pages/login/LoginPage';
 
 export const AuthRoutes = () => {
   return (
     <Routes>
 
-        <Route path='/login' element={""}/>
+        <Route path='/login' element={ <LoginPage/> }/>
         <Route path='/register' element={""}/>
 
     </Routes>
