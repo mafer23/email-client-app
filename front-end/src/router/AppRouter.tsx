b8d603070e99faa0723e224de19288bad54c9ec9
Mateo8585 <42383450+Mateo8585@users.noreply.github.com>
2024-02-20 16:40:44 -0500

    add: rounting system added

--- front-end/src/router/AppRouter.tsx
+++ front-end/src/router/AppRouter.tsx
@@ -1,7 +1,36 @@
+//* Importing react router
+import { Navigate, Route, Routes } from "react-router";
+
+//* Importing routes for each pages
+import { AuthRoutes } from "../auth/routes/AuthRoutes";
+import { DashboardRoutes } from "../dashboard/routes/DashboardRoutes";
+
 export const AppRouter = () => {
+
+  const status: string = 'no-authenticated';
+
   return (
-    <div>
+    <Routes>
         
-    </div>
+        {
+          
+          ( status === 'no-authenticated' )
+          ? (
+            <>
+              <Route path="/auth/*" element={ <AuthRoutes/> }/>
+              <Route path="/*" element={ <Navigate to='/auth/login'/> }/>
+            </>
+          )
+          : (
+            <>
+              <Route path="/emailClient/*" element={ <DashboardRoutes/> }/>
+              <Route path="/*" element={ <Navigate to='/emailClient/dashboard'/> }/>
+            </>
+          )
+
+        }
+
+    </Routes>
   );
+
 }
