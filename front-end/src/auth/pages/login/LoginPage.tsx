82315618e1a1402b7cb4e3d3784381a3a2cfd9e5
Mateo8585 <42383450+Mateo8585@users.noreply.github.com>
2024-02-21 14:13:19 -0500

    add: colors and images added to the proyect

--- front-end/src/auth/pages/login/LoginPage.tsx
+++ front-end/src/auth/pages/login/LoginPage.tsx
@@ -2,8 +2,8 @@ import "./_loginPage.scss";
 
 export const LoginPage = () => {
   return (
-    <div className="loginPage">
-        LoginPage
+    <div className="navBar">
+      
     </div>
   );
 }
