204647964bc7fd3307f1b48fa947a856c3a58802
jcortesj <jcortesj@unal.edu.co>
2024-02-22 12:49:05 -0500

    tech logo

--- front-end/src/dashboard/pages/DashboardPage.tsx
+++ front-end/src/dashboard/pages/DashboardPage.tsx
@@ -4,7 +4,9 @@ export const DashboardPage = () => {
   return (
     <article className="dashboardPage">
       <section className="dashboardPage__leftContainer">
-        Hola
+        <h2 className="dashboardPage__leftContainer__logo">
+          TechEmail+
+        </h2>
       </section>
       <section className="dashboardPage__rightContainer">
         Hola 2
