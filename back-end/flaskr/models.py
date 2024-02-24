184549f48eb14b4848c29305f64bd94fa8b502d8
Oscar699 <odrojasp@udistrital.edu.co>
2024-02-24 14:50:03 -0500

    Added functions to save and get info from db.

--- back-end/flaskr/models.py
+++ back-end/flaskr/models.py
@@ -1,6 +1,6 @@
 from flaskr.db import db
 from sqlalchemy.orm import relationship, mapped_column
-from sqlalchemy import Column, Integer, String, ForeignKey, Text, TIMESTAMP
+from sqlalchemy import Integer, String, ForeignKey, Text, TIMESTAMP
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 
 # Class that represents the "User" table in the database
@@ -14,14 +14,14 @@ class User(db.Model):
     lasttName = mapped_column(String(32))
     password = mapped_column(String(255), nullable=False)
 
-
+    # Retreives all 
     @classmethod
     def get_all_users(cls):
         statement = db.select(cls)
         users = db.session.execute(statement).scalars()
         user_schema = UserSchema()
         return user_schema.get_users(users)
-
+    
     @classmethod
     def get_user_by_userName(cls, userName):
         statement = db.select(cls).filter_by(userName=userName)
@@ -44,11 +44,12 @@ class UserEmail(db.Model):
     email = relationship('Email', back_populates='email_users')
 
     @classmethod
-    def get_user_received_emails(cls, userId):
-        statement = db.select(cls).join(Email, cls.emailId == Email.emailId).where(cls.recipentId == userId)
-        relationship_info = db.session.execute(statement).scalars()
+    def get_all(cls):
+        statement = db.select(cls)
+        useremails = db.session.execute(statement).scalars()
         user_email_schema = UserEmailSchema()
-        return user_email_schema.get_users_emails(relationship_info)
+        return user_email_schema.get_users_emails(useremails)
+    
 
 # Class that represnts the "Email" table in database
 class Email(db.Model):
@@ -67,6 +68,35 @@ class Email(db.Model):
         email = db.session.execute(statement).scalars()
         emal_schema = EmailSchema()
         return emal_schema.get_emails(email)
+    
+    @classmethod
+    def get_user_received_emails(cls, userId):
+        statement = db.select(cls).join(UserEmail, cls.emailId == UserEmail.emailId).where(UserEmail.recipentId == userId)
+        emails = db.session.execute(statement).scalars()
+        email_schema = EmailSchema()
+        return email_schema.get_emails(emails)
+    
+    @classmethod
+    def get_user_sent_emails(cls, userId):
+        statement = db.select(cls).join(UserEmail, cls.emailId == UserEmail.emailId).where(UserEmail.senderId == userId)
+        emails = db.session.execute(statement).scalars()
+        email_schema = EmailSchema()
+        return email_schema.get_emails(emails)
+    
+    @classmethod
+    def get_all(cls):
+        statement = db.select(cls)
+        email = db.session.execute(statement).scalars()
+        emal_schema = EmailSchema()
+        return emal_schema.get_emails(email)
+    
+    @classmethod
+    def save_email(cls, recipent, sender, subject, body):
+        email = cls(subject=subject, body=body)
+        email_users = UserEmail(recipentId=recipent, senderId=sender)
+        email.email_users.append(email_users)
+        db.session.add(email)
+        db.session.commit()
 
 
 """
@@ -80,6 +110,8 @@ an array of dicts, in which each dicht is like the row in db
 class UserSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = User
+        include_relationships = True
+        load_instance = True
     
     # Returns an array with formated information
     def get_users(self, users):
@@ -90,6 +122,8 @@ class UserEmailSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = UserEmail
         include_fk = True
+        load_instane = True
+
     
     # Returns an array with formated information
     def get_users_emails(self, users_emails):
