a0f6ee687b478c06b919853a53845ceff5841103
Oscar699 <odrojasp@udistrital.edu.co>
2024-02-23 22:49:33 -0500

    Replace loggic for db connection configuration

--- back-end/flaskr/__init__.py
+++ back-end/flaskr/__init__.py
@@ -7,6 +7,8 @@ from flask import Flask
 
 from aiosmtpd.controller import Controller
 from smtp.server import SMTPServer
+from flaskr.db import db
+from flaskr.models import User, UserEmail, Email
 
 
 def create_app(test_config=None):
@@ -15,6 +17,7 @@ def create_app(test_config=None):
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        SQLALCHEMY_DATABASE_URI='postgresql://postgres:abc123@localhost/test'
     )
 
     if test_config is None:
@@ -23,6 +26,11 @@ def create_app(test_config=None):
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
+
+    # Initialize and configure the db connection
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
 
     # ensure the instance folder exists
     try:
@@ -34,6 +42,15 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+    
+    # testing the connection with database
+    @app.route('/testDB')
+    def testDB():
+        relationship_info = UserEmail.get_user_received_emails(2)[0]
+        # print(Email.get_email_by_id(relationship_info['emailId']))
+        print(relationship_info)
+
+        return 'Not internal problems at least'
 
     app.register_blueprint(auth.bp)
     app.register_blueprint(email.bp)
