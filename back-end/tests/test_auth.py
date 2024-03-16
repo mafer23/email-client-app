5416d2315ad0fd3d620430cad6cad73996c0daf7
Maria Fernanda Palencia <39041288+mafer23@users.noreply.github.com>
2024-03-16 15:56:35 -0500

    test adicional
    Revisa

--- back-end/tests/test_auth.py
+++ back-end/tests/test_auth.py
@@ -18,7 +18,7 @@ import pytest
                 "username": "test_user_1",
                 "firstname": "test_firstname",
                 "lastname": "test_lastname",
-                "password":""
+         
             },
             [b"Password is required", 400],
         ),
@@ -29,6 +29,35 @@ def test_register_user(client, register_user_input, expected):
     assert response.status_code == expected[1]
     assert expected[0] in response.data
 
+
+@pytest.mark.parametrize(
+    "register_user_input, expected",
+    [
+        (
+            {
+                "username": "test_user1",
+                "firstname": "test_firstname",
+                "lastname": "test_lastname",
+                "password": "testpassword",
+            },
+            [b"You registered successfully", 201],
+        ),
+        (
+            {
+                "username": "test_user_1",
+                "firstname": "test_firstname",
+                "lastname": "test_lastname",
+                "password": ""
+         
+            },
+            [b"Password is required", 400],
+        ),
+    ],
+)
+def test_register_user_return(client, register_user_input, expected):
+    response = client.post("/auth/register", json=register_user_input)
+    assert response.status_code == expected[1]
+    assert expected[0] in response.data
 
 @pytest.mark.parametrize(
     "login_user_input, expected",
